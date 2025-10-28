2fcaadec6acddb10eb1a8b68659ff6393ece19f0
YoungerYang <1059376437@qq.com>
2025-10-28 22:21:34 +0800

    feat(example): 增加示例工程，便于调试

---
+++ examples/example-general-web-project/src/main/java/com/yggdrasil/labs/example/GeneralWebProject.java
@@ -0,0 +1,18 @@
+package com.yggdrasil.labs.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @ClassName: GeneralWebProject
+ * @Description: 通用Web项目示例
+ * @Author: Yang
+ * @Date: 2025/10/28 8:06
+ * @Version: 1.0
+ */
+@SpringBootApplication
+public class GeneralWebProject {
+    public static void main(String[] args) {
+        SpringApplication.run(GeneralWebProject.class, args);
+    }
+}
